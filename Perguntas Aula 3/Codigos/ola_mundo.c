8a0be9ca81b1bf7105cd899d5f94a906ea9e0ef8
arthurbragav <arthurbragav@gmail.com>
2017-04-18 14:43:45 -0300

    remoção de arquivos 2

--- Perguntas Aula 3/Codigos/ola_mundo.c
+++
@@ -1,8 +0,0 @@
-# include <stdio.h>
-
-int main()
-{
-printf ("Ola_mundo");
-return 0;
-
-}
--- Perguntas Aula 3/Codigos/ola_usuario_3.c
+++
@@ -1,8 +0,0 @@
-# include <stdio.h>
-
-int main(int argc, char **argv)
-{
- printf ("Ola %s\n", argv[1]);
- printf("Numero de entradas = %d",argc);
- return 0;
-}
